3b70579d5ba121e8ce0742a6890506e313aa2930
Patel210 <pranavpatel2101997@gmail.com>
2020-10-28 07:15:49 +0530

    Modified the test case to validate the salary change for an employee, which was updated using prepared statement

--- src/test/java/com/capgemini/payrollservicetest/EmployeePayrollServiceTest.java
+++ src/test/java/com/capgemini/payrollservicetest/EmployeePayrollServiceTest.java
@@ -50,6 +50,7 @@ public class EmployeePayrollServiceTest {
 		try {
 			employeePayrollService.updateEmployeeSalary("Terisa", 3000000.0);
 			boolean result = employeePayrollService.isEmployeePayrollInSyncWithDB("Terisa");
+			System.out.println(result);
 			assertTrue(result);
 		} catch (DatabaseException e) {}
 	}
