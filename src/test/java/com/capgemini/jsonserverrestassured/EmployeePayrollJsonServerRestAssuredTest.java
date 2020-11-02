110f7f11481bf12aee6b5edf6d65f16bd72fa18d
Patel210 <pranavpatel2101997@gmail.com>
2020-11-02 17:54:15 +0530

    Added test case to add multiple employees with multi-threading to Json Server using Rest Assured

--- src/test/java/com/capgemini/jsonserverrestassured/EmployeePayrollJsonServerRestAssuredTest.java
+++ src/test/java/com/capgemini/jsonserverrestassured/EmployeePayrollJsonServerRestAssuredTest.java
@@ -3,11 +3,16 @@ package com.capgemini.jsonserverrestassured;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.capgemini.exceptions.DatabaseException;
 import com.capgemini.payrolldata.EmployeePayrollData;
 import com.capgemini.payrollservice.EmployeePayrollService;
 import com.capgemini.payrollservice.EmployeePayrollService.IOService;
@@ -41,6 +46,43 @@ public class EmployeePayrollJsonServerRestAssuredTest {
 		return request.post("/employee-payroll"); 
 	}
 	
+	private Map<Integer, Boolean> addMultipleEmployeesToJsonServer(List<EmployeePayrollData> employeePayrollDataList) {
+		Map<Integer, Boolean> employeeInsertionStatus = new HashMap<Integer, Boolean>();
+		Map<Integer, Boolean> employeeAdditionStatusCodes = new HashMap<Integer, Boolean>();
+		employeePayrollDataList.forEach(employeePayrollData -> {
+			employeeInsertionStatus.put(employeePayrollData.hashCode(), false);
+			Runnable task = () -> {
+				System.out.println("Employee Being added: " + Thread.currentThread().getName());
+				
+				Response response = addEmployeeToJsonServer(employeePayrollData);
+				int statusCode = response.getStatusCode();
+				if (statusCode == 201) {
+					employeeInsertionStatus.put(employeePayrollData.hashCode(), true);
+					String responseAsString = response.asString();
+					JsonObject jsonObject = new Gson().fromJson(responseAsString, JsonObject.class);
+					int id = jsonObject.get("id").getAsInt();
+					employeePayrollData.setId(id);
+					employeeAdditionStatusCodes.put(employeePayrollData.hashCode(), true);
+				}
+				else {
+					employeeAdditionStatusCodes.put(employeePayrollData.hashCode(), false);
+				}
+				System.out.println("Employee added: " + Thread.currentThread().getName());
+			};
+			Thread thread = new Thread(task, employeePayrollData.getName());
+			thread.start();
+		});
+		
+		while (employeeInsertionStatus.containsValue(false)) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return employeeAdditionStatusCodes;
+	}
+	
 	@Test
 	public void givenNewEmployee_OnPost_ShouldMatch201ResponseAndCount() {
 		EmployeePayrollData[] arrayOfEmps = getEmployeeList();
@@ -58,5 +100,29 @@ public class EmployeePayrollJsonServerRestAssuredTest {
 		employeePayrollService.addEmployeeToPayroll(employeePayrollData, IOService.REST_IO);
 		long enteries = employeePayrollService.countEntries(IOService.REST_IO);
 		assertEquals(2, enteries);
+	}
+	
+	@Test
+	public void givenMultipleEmployee_OnPost_ShouldMatch201ResponseAndCount() {
+		EmployeePayrollData[] arrayOfEmps = getEmployeeList();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		List<EmployeePayrollData> employeePayrollDataList = new ArrayList<EmployeePayrollData>() {{
+			add(new EmployeePayrollData(0, "Daksh", "Santa Cruz, Mumbai", 60000,
+				LocalDate.now(), "M".charAt(0), "Capgemini", "Sales"));
+			add(new EmployeePayrollData(0, "Arvind", "Hauz Khas, New Delhi", 50000,
+					LocalDate.now(), "M".charAt(0), "Capgemini", "Production"));
+			add(new EmployeePayrollData(0, "Vaishali", "Whitefield, Bangalore", 45000,
+					LocalDate.now(), "F".charAt(0), "Capgemini", "Finance"));
+		}};
+		
+		Map<Integer, Boolean> employeeAdditionStatusCodes = addMultipleEmployeesToJsonServer(employeePayrollDataList);
+		assertFalse(employeeAdditionStatusCodes.containsValue(false));
+		employeePayrollDataList.forEach(employeePayrollData -> {
+			if(employeeAdditionStatusCodes.get(employeePayrollData.hashCode())) {
+				employeePayrollService.addEmployeeToPayroll(employeePayrollData, IOService.REST_IO);
+			}
+		});
+		long enteries = employeePayrollService.countEntries(IOService.REST_IO);
+		assertEquals(5, enteries);
 	}
 }
